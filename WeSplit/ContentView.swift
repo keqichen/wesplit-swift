6414bc0b69d16a459874409d04d2e0ec79f361c7
Keqi <ckq009@gmail.com>
2023-04-15 22:59:14 +0100

    Complete main functionality, bugs about binding variable to be fixed

--- WeSplit/ContentView.swift
+++ WeSplit/ContentView.swift
@@ -8,11 +8,95 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var checkAmount=0.0
+    @State private var numberOfPeople = 2
+    @State var tipPercentage = 20
+    @FocusState private var amountIsFocused: Bool
+    
+    let tipPercentages = (0..<100)
+    
+    var totalAmount: Double{
+        let tipSelection = Double(tipPercentage)
+        let tipValue = checkAmount/100 * tipSelection
+        let totalAmount = checkAmount + tipValue
+        return totalAmount
+    }
+    
+    var totalPerPerson: Double{
+        let peopleCount = Double(numberOfPeople+2)
+        let tipSelection = Double(tipPercentage)
+        let tipValue = checkAmount/100 * tipSelection
+        let totalPerPerson = (checkAmount + tipValue)/peopleCount
+        return totalPerPerson
+    }
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        NavigationView{
+            Form{
+                Section{
+                    TextField("Amount", value: $checkAmount, format: .currency(code: Locale.current.currencyCode ?? "USD"))
+                        .keyboardType(.decimalPad)
+                        .focused($amountIsFocused)
+                    
+                    Picker("Number of people", selection: $numberOfPeople){
+                        ForEach(2..<100){
+                           Text("\($0) people")
+                        }
+                    }
+                }
+                
+                    Section{
+                    Text("How much tip do you want to leave?")
+                        NavigationLink(destination: TipView()){
+                            Text("Choose your tips")
+                        }
+                    }
+                    
+//                    header: {
+//                        Text("How much tip do you want to leave?")
+//                    }
+//                    .pickerStyle(.segmented)
+//                }
+           
+                
+                Section{
+                    Text(totalAmount, format: .currency(code: Locale.current.currencyCode ?? "USD"))
+                } header: {
+                    Text("Total amount")
+                }
+                
+                Section{
+                    Text(totalPerPerson, format: .currency(code: Locale.current.currencyCode ?? "USD"))
+                } header: {
+                    Text("Amount per person")
+                }
+            }
+            .navigationTitle("WeSplit")
+            .toolbar{
+                ToolbarItemGroup(placement:.keyboard){
+                    Button("Done"){
+                        amountIsFocused = false
+                    }
+                }
+            }
+        }
     }
 }
+    
+struct TipView : View {
+    @Binding var tipPercentage:Int
+    let tipPercentages = (0..<100)
+        var body: some View {
+            NavigationView {
+                Picker("Tip percentage",
+                       selection: $tipPercentage){
+                    ForEach(tipPercentages, id: \.self){
+                        Text($0, format: .percent)
+                    }
+                }
+            }
+        }
+    }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
